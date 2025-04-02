e5208335ada4af210bd6c4cd9712f56dda8e4f46
OMGLAM <cristianowirtz@gmail.com>
2025-04-02 18:56:45 +0000

    Create migration.php

---
+++ migration.php
@@ -0,0 +1,10 @@
+public function up()
+{
+    Schema::create('tasks', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->text('description')->nullable();
+        $table->boolean('status')->default(0);
+        $table->timestamps();
+    });
+}
