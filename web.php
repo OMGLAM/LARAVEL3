f3093b69a35ad741b4a07694f51daa076b9e3a75
OMGLAM <cristianowirtz@gmail.com>
2025-04-02 18:57:53 +0000

    Create web.php

---
+++ web.php
@@ -0,0 +1,6 @@
+use App\Http\Controllers\TaskController;
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/create', [TaskController::class, 'create']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::get('/tasks/{id}', [TaskController::class, 'show']);
